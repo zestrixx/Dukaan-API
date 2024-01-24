0e4da5e8ab0f8cb7476675fade76392af48a8155
Mayank Singh <mayank.edux@gmail.com>
2024-01-25 05:07:06 +0530

    just a formatting

--- DukaanAPI/registration/serializers.py
+++ DukaanAPI/registration/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-# from django.contrib.auth.models import User
 from django.contrib.auth import get_user_model
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
