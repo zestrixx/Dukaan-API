9c370d29728a95462141dd6aa4cb09c269efe3ac
Mayank Singh <mayank.edux@gmail.com>
2024-02-20 01:00:14 +0530

    testing with api

--- DukaanAPI/APIHandler/models.py
+++ DukaanAPI/APIHandler/models.py
@@ -6,7 +6,17 @@ class Orders(models.Model):
     id = models.CharField(primary_key=True, unique=True, max_length=200)
     date = models.DateTimeField(auto_now_add = True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    items = models.JSONField()
 
 class OrderDetails(models.Model):
-    pass
+    order_id = models.ForeignKey(Orders, on_delete = models.CASCADE)
+    items = models.JSONField()
+    customer_first_name = models.CharField(max_length=100)
+    customer_last_name = models.CharField(max_length=100)
+    customer_phone_number = models.IntegerField()
+    address = models.CharField(max_length=200)
+    landmark = models.CharField(max_length=200)
+    zip_code = models.CharField(max_length=200)
+    city = models.CharField(max_length=200)
+    state = models.CharField(max_length=200)
+    country = models.CharField(max_length=200)
+
